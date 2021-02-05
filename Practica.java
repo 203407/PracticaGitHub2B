a008ff42cec3de368024eddf2226cfb0762b4f8d
203407 <203407@ids.upchiapas.edu.mx>
2021-02-05 12:42:50 -0600

    Primera version

---
+++ Practica.java
@@ -0,0 +1,21 @@
+public class Practica{
+    private int patas;
+    private String mandera; 
+    
+    public void setMandera(String mandera) {
+        this.mandera = mandera;
+    }
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+    public String getMandera() {
+        return mandera;
+    }
+    public int getPatas() {
+        return patas;
+    }
+    
+    public void fabricar(){}
+
+    
+}
